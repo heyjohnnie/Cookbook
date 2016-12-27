6c4d8a293f98ef65ffcbbb34ff8e8756a2948325
J. M. Paz <jm.pazro@gmail.com>
2016-12-26 19:00:37 -0600

    Added comments

--- IsPhoneNumber/IsPhoneNumber/IsPhoneNumber.py
+++ IsPhoneNumber/IsPhoneNumber/IsPhoneNumber.py
@@ -1,9 +1,16 @@
+#The isPhoneNumber() function has code that does several checks to see whether the string in text is a valid phone number.
+#If any of these checks fail, the function returns False.
+
 def isPhoneNumber (text):
+    #First the code checks that the string is exactly 12 characters.
     if len(text) != 12:
         return False
+    #Then it checks that the area code (that is, the first three characters in text) consists of only numeric characters
     for i in range (0, 3):
         if not text[i].isdecimal():
             return False
+    # The rest of the function checks that the string follows the pattern of a phone number:
+    #The number must have the first hyphen after the area code.
     if text[3] != "-":
         return False
     for i in range (4, 7):
@@ -14,6 +21,8 @@ def isPhoneNumber (text):
     for i in range (8, 12):
         if not text[i].isdecimal():
             return False
+
+    #If the program execution manages to get past all the checks, it returns True
     return True
 
 #print('415-555-4242 is a phone number:')
